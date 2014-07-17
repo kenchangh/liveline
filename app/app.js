e9cb9c372230e1e43919ea752f0eae1fea5ac3b4
Howard Chan <guanhao3797@gmail.com>
2014-07-17 15:39:23 +0800

    Tidied up project

--- app/app.js
+++ app/app.js
@@ -1,3 +1,8 @@
+/*
+ * Author: Maverick Chan
+ * Project: Live Line
+ */
+
 var express = require('express');
 var path = require('path');
 var favicon = require('static-favicon');
@@ -5,12 +10,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -19,21 +21,25 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'static')));
+
+/// Routes
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
 app.use('/', routes);
 app.use('/users', users);
 
-/// catch 404 and forward to error handler
+/// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
-/// error handlers
+/// Error handlers
 
-// development error handler
+// Development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
@@ -45,8 +51,8 @@ if (app.get('env') === 'development') {
     });
 }
 
-// production error handler
-// no stacktraces leaked to user
+// Production error handler
+// No stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
