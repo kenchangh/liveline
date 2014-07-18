208fe3d4b475fe8e0faabf2a0269267b3ae5ac2d
Howard Chan <guanhao3797@gmail.com>
2014-07-18 15:06:06 +0800

    Style changes and tested ajax request
    The front page will have a signup jumbotron and
    a login form at the top navbar.
    
    I just tested ajax requests, it works well!

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -4,29 +4,14 @@ var router = express.Router();
 /* GET home page. */
 router.get('/signup', function(req, res) {
   // render 404
+  return null;
 });
 
 router.post('/signup', function(req, res) {
-  var db = req.db;
-  var collection = db.get('collection');
-  var uid = req.body.username;
+  var uid = req.body.uid;
   var pw = req.body.password;
-
-  collection.insert({
-    'uid': uid,
-    'pw': pw
-  },
-  function(err, doc) {
-    if (err) {
-      // Render error
-      res.send('There was problem in signing up');
-    }
-    else {
-      res.location('/');
-      res.redirect('/');
-    }
-  });
-
+  console.log(uid + ' ' + pw)
 });
 
 module.exports = router;
+
