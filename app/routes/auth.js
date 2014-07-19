a9fbec6ace88dafcf494bb6731dabf3e80ec67a4
Howard Chan <guanhao3797@gmail.com>
2014-07-19 19:26:39 +0800

    Updated front page

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// No GET access
 router.get('/signup', function(req, res) {
   // render 404
   return null;
 });
 
+// Only executed upon ajax request
 router.post('/signup', function(req, res) {
   var uid = req.body.uid;
-  var pw = req.body.password;
+  var pw = req.body.pw;
   console.log(uid + ' ' + pw)
 });
 
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('front', { title: 'Liveline - A better way to connect' });
 });
 
 module.exports = router;
