53c92dcf60117df7adb73a3e38f944ef7ea78c79
Howard Chan <guanhao3797@gmail.com>
2014-07-20 19:53:41 +0800

    Improved login page

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,8 +6,8 @@ router.get('/', function(req, res) {
   res.render('front', { title: 'Liveline - A better way to connect' });
 });
 
-router.get('/', function(req, res) {
-  res.render('front', { title: 'Liveline - A better way to connect' });
+router.get('/login', function(req, res) {
+  res.render('login', { title: 'Sign in to Liveline' });
 });
 
 module.exports = router;
